63cfa8cdb475a0d8d51d59c161ed63a1a492893a
Yohanna Sium <yohannasium@Yohannas-MacBook-Air.local>
2023-10-12 17:39:24 +0200

    Fixing register issues removing concurrently package using Node Filesystem instead

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const stripe = require('stripe')('sk_test_51NoS4DFDZ1Q35EswtM1dk3lqthdqPfKmU11e8luUDoQh3MLYdV6mOdP8AGQz1CaFDcWOXU6IHj4in3QVAOGXMium00i3OYNCnb');
+const fs = require("fs");
+const stripe = require("stripe")(
+  "sk_test_51NoS4DFDZ1Q35EswtM1dk3lqthdqPfKmU11e8luUDoQh3MLYdV6mOdP8AGQz1CaFDcWOXU6IHj4in3QVAOGXMium00i3OYNCnb"
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,87 +14,81 @@ app.use(cors());
 
 app.get("/products", async (req, res) => {
   const products = await stripe.products.list();
-  res.send(products)
-})
+  res.send(products);
+});
 app.get("/prices", async (req, res) => {
   const prices = await stripe.prices.list();
-  res.send(prices)
-})
+  res.send(prices);
+});
 app.post("/checkout/payment", async (req, res) => {
-  const lineItems = req.body.map(product => ({
+  const lineItems = req.body.map((product) => ({
     price: product.default_price,
     quantity: 1,
-}));
+  }));
 
-try {
+  try {
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: lineItems,
-        mode: 'payment',
-        success_url: 'http://localhost:5173/success', // Replace with your success URL
-        cancel_url: 'http://localhost:5173/checkout',   // Replace with your cancel URL
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: "http://localhost:5173/success", // Replace with your success URL
+      cancel_url: "http://localhost:5173/checkout", // Replace with your cancel URL
     });
 
     res.json({ sessionId: session.id });
-} catch (error) {
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to create checkout session' });
-}
-})
+    res.status(500).json({ error: "Failed to create checkout session" });
+  }
+});
 // Endpoints
 app.post("/register", async function (req, res) {
   //1. Check if inputs are not empty
   const { fullname, email, password } = req.body;
   if (!fullname || !email || !password)
     return res.send("All fields are required");
-  //2. Does the user exist already?
-  const response = await fetch("http://localhost:5000/users");
-  const users = await response.json();
-  const condition = users.filter((user) => user.email === email);
 
-  if (condition.length > 0) {
-    res.status(400).json({ error: "User already exists." });
-  } else {
-    // Skapa en customer i Stripe
-    const customer = await stripe.customers.create({
-      email: email
+  // Skapa en customer i Stripe
+  const customer = await stripe.customers.create({
+    email: email,
+  });
+  //3. Create the user with a hashed password.
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    fs.readFile("db.json", "utf-8", function (err, data) {
+      const usersObject = JSON.parse(data);
+      usersObject["users"].push({
+        fullname,
+        email,
+        password: hash,
+        customerID: customer.id,
+      });
+      fs.writeFileSync("db.json", JSON.stringify(usersObject), "utf-8");
+      res.send(usersObject);
     });
-    //3. Create the user with a hashed password.
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-      fetch("http://localhost:5000/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ fullname, email, password: hash, customerID: customer.id }),
-      })
-        .then((success) => res.send("User created successfully"))
-        .catch((err) => res.send(err));
-    });
-  }
+  });
 });
 
 app.post("/login", async function (req, res) {
   // Find the user in the database
   const { email, password } = req.body;
-  if (!email || !password)
-    return res.send("All fields are required");
+  if (!email || !password) return res.send("All fields are required");
 
-  const response = await fetch("http://localhost:5000/users");
-  const users = await response.json();
-  const user = users.filter((user) => user.email === email)[0];
+  fs.readFile("db.json", "utf-8", function (err, data) {
+    const usersObject = JSON.parse(data);
+    const user = usersObject["users"].filter((u) => u.email === email)[0];
 
- if (user) {
-     // Check if the password is correct
-  bcrypt.compare(password, user.password, function (err, result) {
-    // IS the password correct then result = true
-    if (result == true) {
-      res.send({ signedIn: true });
-    } else {
-      res.send({ signedIn: false });
+    if (user) {
+      // Check if the password is correct
+      bcrypt.compare(password, user.password, function (err, result) {
+        // IS the password correct then result = true
+        if (result == true) {
+          res.send({ signedIn: true });
+        } else {
+          res.send({ signedIn: false });
+        }
+      });
     }
   });
- }
 });
 
 app.listen(4000, () => {
